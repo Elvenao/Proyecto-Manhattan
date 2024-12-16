159da1a6edf8d2953295a74f3ebf1bdf65ac4eb9
Elvenao <emhdzss@gmail.com>
2024-12-16 14:52:14 -0600

    Role Session Added

--- _controller/AjaxLogin.php
+++ _controller/AjaxLogin.php
@@ -19,12 +19,13 @@ try {
 
 
     $model = new MainModel();
-    $usuario = $model->getDataRows("usuario", ["user", "password"], "user = ?;", [$user]);
+    $usuario = $model->getDataRows("usuario", ["user", "password","rol_id"], "user = ?;", [$user]);
 
     if ($usuario && $usuario[0]['user'] === $user && $usuario[0]['password'] === $pass) {
         echo json_encode(["resultado" => 1, "mensaje" => "Inicio de sesion exitoso"]);
         setcookie("LoggedIN", "true", time()+7200, "/");
         $_SESSION["LoggedIN"] = $user;
+        $_SESSION["rol"] = $usuario[0]["rol_id"];
         exit;
     } else {
         echo json_encode(["resultado" => 0, "mensaje" => "Credenciales incorrectas"]);
