1c370242831b31f9f2974d09dcbeb2a8b7d61b5f
Elvenao <emhdzss@gmail.com>
2025-01-13 17:32:11 -0600

    renderPerfil drop

--- js/ctrlPerfil.js
+++
@@ -1,9 +0,0 @@
-function renderPerfil(){
-<<<<<<< HEAD
-    window.history.pushState(null, "Perfil", "/ElGatoHambriento/perfil/");
-=======
-    window.history.pushState(null, "Perfil", "perfil");
->>>>>>> origin/Altern
-    location.reload(true); 
-}
-
