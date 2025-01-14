1dd61035ec87246e1cb874cc2e2d6d2234692d64
Elvenao <emhdzss@gmail.com>
2025-01-13 19:32:35 -0600

    global edited

--- config/global.php
+++ config/global.php
@@ -1,5 +1,7 @@
 <?php
-    define("SITE_URL", "http://localhost/ElGatoHambriento/");
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+    $base_url = $protocol . $_SERVER['HTTP_HOST'] . '/ElGatoHambriento/';
+    define("SITE_URL", $base_url);
     define("RUTA_DEFAULT_LOGGED", "reportes/");
     define("RUTA_DEFAULT_UNLOGGED","login/");
     define("CLAVE_SECRETA","e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
