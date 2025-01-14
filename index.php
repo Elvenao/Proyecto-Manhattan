e122146c2307356e7d50d1148c8c4a91cda0cef1
Elvenao <emhdzss@gmail.com>
2025-01-13 23:31:33 -0600

    Minor changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         
     }
     
-
+    $titulo;
     $queryString = str_ends_with($queryString, "/") ? $queryString : $queryString."/";
 
     $peticion = explode("/",$queryString);
@@ -39,25 +39,31 @@
         $rol = $model->getDataRows('usuario',['rol_id'],'user = ?',[$_SESSION["usr"]]);
         switch($controlador){
             case "reportes":
+                $titulo = "Reportes";
                 require_once "_controller/ReportesController.php";
                 $ctrl = new ReportesController();
             break;
             case "notas":
                 switch($lista){
                     case "":
+                        $titulo = "Notas";
                         require_once "_controller/NotasMenuController.php";
                         $ctrl = new NotasMenuController();
                     break;
                     case "pos":
+                        $titulo = "Punto de Venta";
                         require_once "_controller/posController.php";
                         $ctrl = new posController();
                     break;
                     case "consultar":
+                        
                         if($accion == ""){
+                            $titulo = "Consultar nota";
                             require_once "_controller/ConsultarNotasController.php";
                             $ctrl = new ConsultarNotasController();
                         }else if($accion == "vernota"){
                             if($id != ''){
+                                $titulo = "Consultar nota - ". $id;
                                 require_once "_controller/verNotaController.php";
                                 $ctrl = new verNotaController($id);
                             }
@@ -69,18 +75,22 @@
             case "inventario":
                 switch($lista){
                     case "" :
+                        $titulo = "Inventario";
                         require_once "_controller/InventarioMenuController.php";
                         $ctrl = new InventarioMenuController();
                     break;
                     case "almacen":
                         if($accion == ""){
+                            $titulo = "Almacen";
                             require_once "_controller/ListaInventarioController.php";
                             $ctrl = new ListaInventarioController();
                         }else if($accion == "agregar"){
+                            $titulo = "Agregar a Almacen";
                             require_once "_controller/agregarAlmacenController.php";
                             $ctrl = new MtoInventarioController();
                         }else if($accion == 'editar'){
                             if($id != ''){
+                                $titulo = "Editar Suministro - ". $id;
                                 require_once "_controller/editarAlmacenController.php";
                                 $ctrl = new editarAlmacen($id);
                             }else{
@@ -96,10 +106,12 @@
                     break;
                     case "productos":
                         if($accion == ""){
+                            $titulo = "Productos";
                             require_once "_controller/ListaProductosController.php";
                             $ctrl = new ListaProductosController();
                         }else if($accion == "editar"){
                             if($id != ''){
+                                $titulo = 'Editar Producto - '.$id;
                                 require_once "_controller/editarProductoController.php";
                                 $ctrl = new editarProducto($id);
                             }else{
@@ -107,6 +119,7 @@
                                 die();
                             }
                         }else if($accion == "agregar"){
+                            $titulo = "Agregar Producto";
                             require_once "_controller/agregarProductoController.php";
                             $ctrl = new agregarProductoController();
                         }
@@ -134,6 +147,7 @@
             break;
             case "perfil":
                 if($lista == ""){
+                    $titulo = 'Perfil';
                     require_once "_controller/perfilController.php";
                     $ctrl = new perfilController();
                 } else{
@@ -144,10 +158,12 @@
             case "configuracion":
                 if($rol[0]['rol_id'] == 1){
                     if($lista == ""){
+                        $titulo = "Configuracion";
                         require_once "_controller/ConfiguracionController.php";
                         $ctrl = new configuracionController();
                     }elseif($lista == "editar"){
                         if($accion != $_SESSION["usr"] && $accion != ""){
+                            $titulo = "Editar ".$accion;
                             require_once "_controller/editarUsuario.php";
                             $ctrl = new editarUsuario($accion);
                         }else{
@@ -156,6 +172,7 @@
                         } 
                     }elseif($lista == "agregar"){
                         if($accion == ""){
+                            $titulo = "Agregar Usuario";
                             require_once "_controller/agregarUsuarioController.php";
                             $ctrl = new agregarUsuarioController();
                         }else{
@@ -173,12 +190,6 @@
                     die(); 
                 }
                 break;
-            case "perfil":
-                if($accion == ""){
-                    require_once "_controller/perfilController.php";
-                    $ctrl = new perfilController();
-                }
-            break;
             default:
                 include "_view/404.html";
                 die();
